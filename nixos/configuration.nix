c791d9353a7b1356cbc1c2759f767bff7a53930b
Domagoj Miskovic <dooygoy@gmail.com>
2023-04-20 14:44:49 +0200

    defaultPackage is no longer supported in flakes

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -139,7 +139,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget vscode ranger unzip unrar git neofetch htop cowsay cmatrix
     python3Full nodePackages.typescript nodejs helix nerdfonts starship
-    neovim-flake.defaultPackage.${pkgs.system}
+    inputs.neovim-flake.packages.${pkgs.system}.default 
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
