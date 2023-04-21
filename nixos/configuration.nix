9907e680da036454fc0e1cf9aa505db4919f10df
Domagoj Miskovic <dooygoy@gmail.com>
2023-04-21 23:41:16 +0200

    gnome-tweaks

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -137,7 +137,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget vscode ranger unzip unrar git neofetch htop cowsay cmatrix
+    wget vscode ranger unzip unrar git neofetch htop cowsay cmatrix gnome-tweaks
     python3Full nodePackages.typescript nodejs helix nerdfonts starship
     inputs.neovim-flake.packages.${pkgs.system}.default ripgrep nixpkgs-review 
   ];
