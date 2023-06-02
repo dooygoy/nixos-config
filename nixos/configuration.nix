574a16db1c81304e24995763f864ab5c8565de90
Domagoj Miskovic <dooygoy@gmail.com>
2023-06-02 10:36:50 +0200

    change from gnome to plasma. Gnome still wonky a bit

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -94,8 +94,8 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
